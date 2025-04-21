6a3c5a0ff9a19669a9a001dfbf346ab121c1920e
Karanini <karas.benjamin@gmail.com>
2025-04-21 23:47:54 +0200

    split structure

---
+++ libft/ft_split_structure.c
@@ -0,0 +1,26 @@
+char **ft_split(const char *s, char c)
+{
+   /* allocate an array big enough to hold all the words in s */
+   /* loop over the string and find the start of the word */
+      /* find the end of the word */
+         /* copy the world at the first free index in our words array */
+   /* return our words array */   
+}
+
+int word_count(/* whatever parameter you need */)
+{
+   /* find and return the number of words in the string */
+}
+
+void ft_free(/* whatever argument you need */)
+{
+   /* free EVERYTHING you allocated */
+   /* each element of the array as well as the array */
+}
+
+char *fill_word(/* whatever argument you need */)
+{
+   /* allocate enough room for the word */
+   /* copy the word into the memory you allocated above */
+   /* return the allocated word */
+}

7c8cce7dcd3b9a05eab8c752a235f305d106e693
bkaras-g <bkaras-g@student.42.fr>
2025-08-14 17:07:53 +0200

    single pipe memleaks and tests KO

--- pipex/Sources/tunelling_multipipe.c
+++ pipex/Sources/tunelling_multipipe.c
@@ -6,7 +6,7 @@
 /*   By: bkaras-g <bkaras-g@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/14 13:16:44 by bkaras-g          #+#    #+#             */
-/*   Updated: 2025/08/14 16:48:35 by bkaras-g         ###   ########.fr       */
+/*   Updated: 2025/08/14 17:06:26 by bkaras-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -87,13 +87,13 @@ static void	ft_exec_child(int fd_infile, int fd_outfile, t_cmd *cmd,
 	}
 	if (ft_close_unused_fdes(fd_infile, fd_outfile, head) == -1)
 		exit(EXIT_FAILURE);
-	printf("cmd->cmd : %s\n", cmd->cmd);
-	int i = 0;
-	while (cmd->argv[i])
-		{
-			ft_printf("   argv[%d] : %s   ", i, cmd->argv[i]);
-			i++;
-		}
+	// printf("cmd->cmd : %s\n", cmd->cmd);
+	// int i = 0;
+	// while (cmd->argv[i])
+	// 	{
+	// 		ft_printf("   argv[%d] : %s   ", i, cmd->argv[i]);
+	// 		i++;
+	// 	}
 	if (execve(cmd->cmd, cmd->argv, NULL) == -1)
 	{
 		if (errno == ENOENT)
@@ -103,6 +103,8 @@ static void	ft_exec_child(int fd_infile, int fd_outfile, t_cmd *cmd,
 		ft_lstclear(&cmd);
 		exit(127);
 	}
+	ft_lstclear(&head);
+	exit(EXIT_SUCCESS);
 }
 
 // void	ft_exec_parent(int fd_infile, int fd_outfile, t_cmd *head)
@@ -117,16 +119,16 @@ static void	ft_exec_child(int fd_infile, int fd_outfile, t_cmd *cmd,
 
 static int	ft_close_unused_fdes(int fd_infile, int fd_outfile, t_cmd *cmd)
 {
-	ft_printf("fd_infile %d\n", fd_infile);
-	ft_printf("fd_outfile %d\n", fd_outfile);
+	// ft_printf("fd_infile %d\n", fd_infile);
+	// ft_printf("fd_outfile %d\n", fd_outfile);
 	if ((fd_infile >= 0 && close(fd_infile) == -1) ||
 		(fd_outfile >= 0 && close(fd_outfile) == -1))
 		return (perror("pipex: close fd_files"), -1);
 	while (cmd->next)
 	{
-		ft_printf("cmd node %s\n", cmd->cmd);
-		ft_printf("pfd[0] : %d\n", cmd->pfd[0]);
-		ft_printf("pfd[1] : %d\n", cmd->pfd[1]);
+		// ft_printf("cmd node %s\n", cmd->cmd);
+		// ft_printf("pfd[0] : %d\n", cmd->pfd[0]);
+		// ft_printf("pfd[1] : %d\n", cmd->pfd[1]);
 		if ((cmd->pfd[0] >= 0 && close(cmd->pfd[0]) == -1) ||
 			(cmd->pfd[1] >= 0 && close(cmd->pfd[1]) == -1))
 			return (perror("pipex: close pfd"), -1);

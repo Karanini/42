d42c6d0d518fe341b3f45c7eb7c92985b5b1d87e
bkaras-g <bkaras-g@student.42.fr>
2025-08-15 14:03:24 +0200

    testing w/o prints

--- pipex/Sources/main.c
+++ pipex/Sources/main.c
@@ -6,7 +6,7 @@
 /*   By: bkaras-g <bkaras-g@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/05 11:05:57 by bkaras-g          #+#    #+#             */
-/*   Updated: 2025/08/14 15:37:43 by bkaras-g         ###   ########.fr       */
+/*   Updated: 2025/08/15 10:46:00 by bkaras-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,12 +33,12 @@ int	main(int argc, char *argv[], char *env[])
 		|| ft_check_path(&(head->next->cmd), env) == -1 || !head->next->cmd)
 		return (ft_lstclear(&head), 1);
 	// if (ft_check_path(head, env) == -1 || )
-	ft_print_list_complete(head);
+	// ft_print_list_complete(head);
 	// ft_putstr_fd("on arrive ici ?\n", 1);
 	if (ft_create_pipes(head, argc) == -1)
 		return (ft_lstclear(&head), 1);
-	ft_printf("\nAfter creating pipes:\n");
-	ft_print_list_complete(head);
+	// ft_printf("\nAfter creating pipes:\n");
+	// ft_print_list_complete(head);
 	if (ft_fork(fd_infile, fd_outfile, head) == -1)
 		return (ft_lstclear(&head), 1);
 	ft_lstclear(&head);
--- pipex/Sources/tunelling_multipipe.c
+++ pipex/Sources/tunelling_multipipe.c
@@ -6,7 +6,7 @@
 /*   By: bkaras-g <bkaras-g@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/14 13:16:44 by bkaras-g          #+#    #+#             */
-/*   Updated: 2025/08/14 17:06:26 by bkaras-g         ###   ########.fr       */
+/*   Updated: 2025/08/15 12:11:16 by bkaras-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,15 +51,15 @@ int	ft_fork(int fd_infile, int fd_outfile, t_cmd *cmd)
 			return (perror("pipex: ft_fork_exec"), -1);
 		if (cmd->pid == 0)
 			ft_exec_child(fd_infile, fd_outfile, cmd, head);
-		printf("pid %d finished\n", cmd->pid);
+		// printf("pid %d finished\n", cmd->pid);
 		cmd = cmd->next;
 	}
 	ft_close_unused_fdes(fd_infile, fd_outfile, head);
 	while (head)
 	{
-		printf("waiting for pid %d\n", head->pid);
+		// printf("waiting for pid %d\n", head->pid);
 		waitpid(head->pid, NULL, 0);
-		printf("finished waiting\n");
+		// printf("finished waiting\n");
 		head = head->next;
 	}
 	return (0);
@@ -70,13 +70,13 @@ static void	ft_exec_child(int fd_infile, int fd_outfile, t_cmd *cmd,
 {
 	if (cmd->first)
 	{
-		printf("cmd %s is first\n", cmd->cmd);
+		// printf("cmd %s is first\n", cmd->cmd);
 		dup2(fd_infile, STDIN_FILENO); // protect the dup2 ?
 		dup2(cmd->fd_out, STDOUT_FILENO);
 	}
 	else if (cmd->next == NULL)
 	{
-		printf("cmd %s is last\n", cmd->cmd);
+		// printf("cmd %s is last\n", cmd->cmd);
 		dup2(fd_outfile, STDOUT_FILENO);
 		dup2(cmd->fd_in, STDIN_FILENO);
 	}
@@ -94,17 +94,13 @@ static void	ft_exec_child(int fd_infile, int fd_outfile, t_cmd *cmd,
 	// 		ft_printf("   argv[%d] : %s   ", i, cmd->argv[i]);
 	// 		i++;
 	// 	}
-	if (execve(cmd->cmd, cmd->argv, NULL) == -1)
-	{
-		if (errno == ENOENT)
-			ft_putstr_fd("pipex: tunelling: command not found\n", 2);
-		else
-			perror("pipex: tunelling");
-		ft_lstclear(&cmd);
-		exit(127);
-	}
-	ft_lstclear(&head);
-	exit(EXIT_SUCCESS);
+	execve(cmd->cmd, cmd->argv, NULL);
+	if (errno == ENOENT)
+		ft_putstr_fd("pipex: tunelling: command not found\n", 2);
+	else
+		perror("pipex: tunelling");
+	ft_lstclear(&cmd);
+	exit(127);
 }
 
 // void	ft_exec_parent(int fd_infile, int fd_outfile, t_cmd *head)

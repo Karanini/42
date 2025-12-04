f273943e3a4105f6ec5e672de13a1d413cc648cd
bkaras-g <bkaras-g@student.42.fr>
2025-12-04 18:10:33 +0100

    exam03 training rip wip

--- training/exam03/rip_training.c
+++ training/exam03/rip_training.c
@@ -6,19 +6,21 @@
 /*   By: bkaras-g <bkaras-g@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/27 16:00:19 by bkaras-g          #+#    #+#             */
-/*   Updated: 2025/11/27 17:25:51 by bkaras-g         ###   ########.fr       */
+/*   Updated: 2025/12/04 18:07:05 by bkaras-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-int	ft_check_balance(char *str, int start)
+int	ft_strlen(char *str);
+
+int	ft_check_balance(char *str)
 {
 	int	i;
 	int	open;
 	int	close;
 
-	i = start;
+	i = 0;
 	open = 0;
 	close = 0;
 	while (str[i])
@@ -36,34 +38,23 @@ int	ft_check_balance(char *str, int start)
 	return (open - close);
 }
 
-void	ft_rec_rip(char *str, int i)
+void	ft_rec_rip(char *str, int i, int len)
 {
 	int	bal;
 
-	bal = ft_check_balance(str, i);
-	if (!bal)
-	{
-		puts(str);
+	if (i > len)
 		return ;
-	}
-	else if (bal > 0)
+	bal = ft_check_balance(str);
+	if (!bal)
+		puts(str);
+	else
 	{
-		while (str[i] && str[i] != '(')
-			i++;
-		if (!str[i])
-			return ;
-		ft_rec_rip(str, i + 1);
-		str[i] = ' ';
-		ft_rec_rip(str, i + 1);
-		// if (!str[i])
-		// 	return ;
-		// else
-		// 	str[i] = ' ';
-		// while (str[i])
-		// {
-		// 	ft_rec_rip(str, i);
-		// 	i++;
-		// }
+		if (str[i] == '_')
+			str[i] = '(';
+		ft_rec_rip(str, i + 1, len);
+		if (str[i] == '(')
+			str[i] = '_';
+		ft_rec_rip(str, i + 1, len);
 	}
 }
 
@@ -80,12 +71,13 @@ int	ft_strlen(char *str)
 int	main(int argc, char *argv[])
 {
 	int	i;
-	// int	len;
+	int	len;
 
 	if (argc != 2)
 		return (1);
 	i = 0;
-	// len = ft_strlen(argv[1]);
-	ft_rec_rip(argv[1], i);
+	len = ft_strlen(argv[1]);
+	printf("balance=%d\n", ft_check_balance(argv[1]));
+	ft_rec_rip(argv[1], i, len);
 	return (0);
 }
